934e6e4d547d6eb1486d7fff76b0d1f96f80f3f5
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-04-05 10:46:58 +0800

    Add Postman collection and env

--- app/routes/profile/views.py
+++ app/routes/profile/views.py
@@ -1,10 +1,17 @@
-from flask_restful import Resource
 from http import HTTPStatus
+
+from flask import request
+from flask_restful import Resource
 
 
 class ProfilesApi(Resource):
     def get(self):
         return {"message": "profile list"}, HTTPStatus.OK
+
+    def post(self):
+        body = request.get_json()
+        return {"message": "requested to profiles API", "body": body}, HTTPStatus.OK
+
 
 class ProfileApi(Resource):
     def get(self, profile_id):
