5c0ab80b0e3e0a345660f8750f26b4b170245d73
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-04-05 13:44:27 +0800

    Add initial profile model

--- app/__init__.py
+++ app/__init__.py
@@ -26,9 +26,12 @@ def register_blueprints(app: Flask):
 
 
 def init_db(app: Flask):
+    db.app = app
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
+
+    migrate = Migrate(compare_type=True)
 
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app import db
+
+from .profile import Profile
