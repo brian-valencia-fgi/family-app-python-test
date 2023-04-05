229f4478a4da0ff27353c6571bb2a9f7bc5f331d
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-04-05 10:34:54 +0800

    Add routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from http import HTTPStatus
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route("/")
+    def main():
+        {
+            "message": "API is working fine!"
+        }, HTTPStatus.OK
+
+    @app.route("/profiles/<profile_id>")
+    def get_profile(profile_id):
+        return {
+            "message": f"get profile number {profile_id}"
+        }, HTTPStatus.OK
+
+    @app.route('/profiles/')
+    def get_profiles():
+        return {
+            "message": "get profiles list"
+        }, HTTPStatus.OK
+
     return app
