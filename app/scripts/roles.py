c4b351e906d5b3dc5ea43d129d01e7e565b38ad3
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-05-02 17:40:00 +0800

    Add ID to seeded roles

--- app/scripts/roles.py
+++ app/scripts/roles.py
@@ -6,7 +6,8 @@ def seed_roles_helper():
     roles = [
         Role(id=1, role_name="user"),
         Role(id=2, role_name="admin"),
-        Role(id=3, role_name="super-admin")
+        Role(id=3, role_name="super-admin"),
+        Role(role_name="super-super-admin")
     ]
     for role in roles:
         print("Adding role", role.role_name)
