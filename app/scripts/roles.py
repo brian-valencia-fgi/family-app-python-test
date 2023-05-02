d00eca5093c3dd3934872230eca13b57641f4f6f
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-05-02 21:28:21 +0800

    Fix pk violation

--- app/scripts/roles.py
+++ app/scripts/roles.py
@@ -6,8 +6,7 @@ def seed_roles_helper():
     roles = [
         Role(id=1, role_name="user"),
         Role(id=2, role_name="admin"),
-        Role(id=3, role_name="super-admin"),
-        Role(role_name="super-super-admin")
+        Role(id=3, role_name="super-admin")
     ]
     for role in roles:
         print("Adding role", role.role_name)
