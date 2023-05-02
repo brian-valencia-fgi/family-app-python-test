458bc73f4e45e4406f2ca961ee920e5f4b066782
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-05-03 02:56:54 +0800

    Update before decorators endpoint to use marshmallow

--- app/routes/decorators_test/views.py
+++ app/routes/decorators_test/views.py
@@ -3,17 +3,34 @@ from flask_apispec import MethodResource
 from flask_restful import Resource
 from app.core import decorators_test as decorators_test_core
 from app.utils.decorators import request_deserializer, response_serializer
-from app.schemas.decorators_test import DecoratorsTestApiSchemas
+from app.schemas.decorators_test import DecoratorsTestApiSchemas, DecoratorsTestPostRequest, DecoratorsTestPostResponse
 
 
 class BeforeDecoratorsTestApi(Resource, MethodResource):
+    # Use doc decorator here...
+    # @doc(...)
     def post(self):
         body = request.get_json()
-        response = decorators_test_core.do_stuff(body['a'], body['b'], body['c'])
-        return response # what does response look like?
+        # Will throw a ValidationError if any
+        processed_body = DecoratorsTestPostRequest().load(body)
+        
+        # what are DecoratorsTestPostRequest's params again?...
+        # no autocomplete :(
+        response = decorators_test_core.do_stuff(
+            processed_body['a'], processed_body['b'], processed_body['c'])
+        
+        # dump the response as you return it
+        return DecoratorsTestPostResponse().dump(response)
+
 
 class AfterDecoratorsTestApi(Resource, MethodResource):
+    # these two will eventually also add docs!
     @request_deserializer(DecoratorsTestApiSchemas.PostRequest)
     @response_serializer(DecoratorsTestApiSchemas.PostResponse)
+    # processed payload injected into the function's params by the request deserializer!
     def post(self, payload: DecoratorsTestApiSchemas.PostRequest):
+        # function has autocomplete! lets you know it expects a DecoratorsTestApiSchemas.PostRequest
+        # view the definition for it or rely on autocomplete to give hints for available fields
+        
+        # automatically dumped by the decorator!
         return decorators_test_core.do_stuff_with_models(payload)
--- app/schemas/decorators_test.py
+++ app/schemas/decorators_test.py
@@ -1,14 +1,28 @@
 from dataclasses import dataclass
+from marshmallow import fields, Schema
 
+
+# Schemas used by the Resource AFTER applying the decorators
 class DecoratorsTestApiSchemas:
     @dataclass
     class PostRequest:
         a: int
         b: str
         c: float
-    
+
     @dataclass
     class PostResponse:
         message: str
         body: 'DecoratorsTestApiSchemas.PostRequest'
-    
+
+
+# Schemas used by the Resource BEFORE applying the decorators
+class DecoratorsTestPostRequest(Schema):
+    a = fields.Int(required=True)
+    b = fields.Str(required=True)
+    c = fields.Float(required=True)
+
+
+class DecoratorsTestPostResponse(Schema):
+    message = fields.Str(required=True)
+    body = fields.Nested(DecoratorsTestPostRequest())
