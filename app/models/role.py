a066d0196e116c67eb3a2b6bb7c576410ccf1aa9
brian-valencia-fgi <brian.valencia@focusglobalinc.com>
2023-05-02 17:26:28 +0800

    Update role make name unique

--- app/models/role.py
+++ app/models/role.py
@@ -6,7 +6,7 @@ from app import db
 class Role(db.Model):
     __tablename__ = 'roles'
     id = Column(Integer, primary_key=True)
-    role_name = Column(String)
+    role_name = Column(String, unique=True)
     date_added = Column(DateTime, server_default=func.now())
     date_updated = Column(DateTime, server_default=func.now())
     users_in_role = db.relationship('Profile', back_populates='role')
